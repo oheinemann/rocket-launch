986a8f26c40898b723d1de9c35c7713c8232166d
Oliver Heinemann <kontakt@oliver-heinemann.net>
2023-01-25 17:07:26 +0100

    Trying to optimize the wsl2 install

--- wsl2-install.ps1
+++ wsl2-install.ps1
@@ -392,15 +392,17 @@ if ($rebootRequired) {
         $pass = Read-Host 'What is your password?' -AsSecureString
         if ($distro.sideloadreqd){
             if (Check-Sideload){
-                $processOptions = @{
-                    FilePath = $Distro.winpe
-                    ArgumentList = ' config --default-user root'
-                }
+                Start-Process $Distro.winpe -Wait
+                wsl.exe -d $Distro.Name -u root useradd --shell /bin/bash --badnames --create-home --user-group --groups adm,dialout,cdrom,floppy,sudo,audio,dip,video,plugdev,netdev --password "$pass" $user
+                wsl.exe -d $Distro.Name -u root -e bash -c "touch /etc/wsl.conf && if ! fgrep '[boot]' /etc/wsl.conf >/dev/null; then printf '\n[boot]\nsystemd=true\n' >>/etc/wsl.conf; fi"
+                wsl.exe -d $Distro.Name -u root -e bash -c "touch /etc/wsl.conf && if ! fgrep '[user]' /etc/wsl.conf >/dev/null; then printf '\n[user]\ndefault=$user\n' >>/etc/wsl.conf; fi"
             }
         } else {
-#            wsl.exe -d $Distro.Name -u root useradd --badnames --create-home --user-group --groups adm,dialout,cdrom,floppy,sudo,audio,dip,video,plugdev,netdev --password "$pass" $user
-#            wsl.exe -d $Distro.Name -u root -e bash -c "touch /etc/wsl.conf && if ! fgrep '[boot]' /etc/wsl.conf >/dev/null; then printf '\n[boot]\nsystemd=true\n' >>/etc/wsl.conf; fi"
-#            wsl.exe -d $Distro.Name -u root -e bash -c "touch /etc/wsl.conf && if ! fgrep '[user]' /etc/wsl.conf >/dev/null; then printf '\n[user]\ndefault=$user\n' >>/etc/wsl.conf; fi"
+            Start-Process $Distro.winpe -Wait
+
+            wsl.exe -d $Distro.Name -u root useradd --shell /bin/bash --badnames --create-home --user-group --groups adm,dialout,cdrom,floppy,sudo,audio,dip,video,plugdev,netdev --password "$pass" $user
+            wsl.exe -d $Distro.Name -u root -e bash -c "touch /etc/wsl.conf && if ! fgrep '[boot]' /etc/wsl.conf >/dev/null; then printf '\n[boot]\nsystemd=true\n' >>/etc/wsl.conf; fi"
+            wsl.exe -d $Distro.Name -u root -e bash -c "touch /etc/wsl.conf && if ! fgrep '[user]' /etc/wsl.conf >/dev/null; then printf '\n[user]\ndefault=$user\n' >>/etc/wsl.conf; fi"
         }
     } else {
         $wslselect = ""
